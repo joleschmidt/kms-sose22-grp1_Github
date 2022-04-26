b3d3befdc7f408bbbad14374a56f001796786754
joleschmidt <janoleschmidt@outlook.com>
2022-04-26 18:38:46 +0200

    getTodos & renderTodos

--- src/client.ts
+++ src/client.ts
@@ -1,17 +1,34 @@
 
 
-function getFestivals() {
+function getTodos() {
 
     $.ajax({
         url: '/todos',
         type: 'GET',
         dataType: 'json',
         success: (response) => {
-            sortFestivalarray = response.festivalList;
+            //renderTodos(response.todoList);
         },
         error: (jqXHRresponse) => {
-            renderMessage(jqXHRresponse.responseJSON.message);
+            console.log(jqXHRresponse.responseJSON.message);
         },
     }).then(() => {
     });
 }
+
+
+function renderTodos () {
+    //const festivalTableBody: JQuery = $('#alle_festivals');
+    //const searchList: JQuery = $('#festival_search_list');
+    // Delete the old table of users from the DOM
+    
+    for(const todo of todos) {
+
+    }
+}
+
+//Main Callback
+$(() => {
+    getTodos();
+});
+
