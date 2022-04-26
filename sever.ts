98a99a0c72f95d916efd3e27e68bfa6a01e4bf4b
davidb <david.bausch@mni.thm.de>
2022-04-26 18:19:12 +0200

    setup server

---
+++ sever.ts
@@ -0,0 +1,9 @@
+import express = require("express")
+import {Request} from "express"
+import {Response} from "express";
+
+//define and start server
+const app = express();
+app.listen(8080, "localhost", function () {
+    console.log('Server läuft! http://localhost:8080/')
+})
