b10730453238b7318c8b4404780b2ae261463a4d
davidb <david.bausch@mni.thm.de>
2022-04-26 18:21:56 +0200

    added package.json

--- sever.ts
+++ sever.ts
@@ -2,8 +2,10 @@ import express = require("express")
 import {Request} from "express"
 import {Response} from "express";
 
+
 //define and start server
 const app = express();
 app.listen(8080, "localhost", function () {
     console.log('Server läuft! http://localhost:8080/')
 })
+
