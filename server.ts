5dd954895cbd0e4f0301b2f55ea4141045c8797c
davidb <david.bausch@mni.thm.de>
2022-04-28 20:46:19 +0200

    prepared get all users route, can be finalized when class is added and imported

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express = require("express")
 import {Request} from "express";
 import {Response} from "express";
 import * as mysql from "mysql";
-import { Connection, MysqlError } from 'mysql';
+import {MysqlError} from 'mysql';
 
 
 //define and start server
@@ -18,9 +18,12 @@ const database: mysql.Connection = mysql.createConnection({
     user: "root"
 });
 
-database.connect((err: MysqlError) => { if (err) {
-    console.log('Database connection failed: ', err); } else {
-    console.log('Datenbank läuft'); }
+database.connect((err: MysqlError) => {
+    if (err) {
+        console.log('Database connection failed: ', err);
+    } else {
+        console.log('Datenbank läuft');
+    }
 });
 
 
@@ -32,7 +35,7 @@ app.use('/jquery', express.static(basedir + '/node_modules/jquery/dist'));
 app.use('/bootstrap', express.static(basedir + '/node_modules/bootstrap/dist'));
 
 
-app.post('/aufgabe',(req:Request,res:Response)=>{
+app.post('/aufgabe', (req: Request, res: Response) => {
     //Aufgabe erstellen
 
     const aufgabe: string = req.body.aufgabe;
@@ -42,40 +45,64 @@ app.post('/aufgabe',(req:Request,res:Response)=>{
         // Create new aufgabe
         let data: [string] = [aufgabe];
 
-        let query:string= 'INSERT INTO aufgaben (aufgabe)'+ 'VALUES (?);';
+        let query: string = 'INSERT INTO aufgaben (aufgabe)' + 'VALUES (?);';
         /*SQL Aufgabe erstellen:
             INSERT INTO aufgaben (name, prioritaet) VALUES (?, 1)
         -Erwartet Name als String
         */
 
 
-
-        database.query(query,data,(err:MysqlError, result:any)=>{
-            if (err|| result === null){
+        database.query(query, data, (err: MysqlError, result: any) => {
+            if (err || result === null) {
                 //Query could not be executed
                 res.status(500).send({
-                    message:'Database request failed'+ err,
+                    message: 'Database request failed' + err,
                 });
             } else {
                 //die Aufgabe was created
                 res.status(200).send({
-                    message:'Successfully aufgabe created ',
-                    aufgabe:result.aufgabe
+                    message: 'Successfully aufgabe created ',
+                    aufgabe: result.aufgabe
                 });
             }
         });
-    }else {
+    } else {
         res.status(400).send({
-            message:'Not all mandatory fields are filled in',
+            message: 'Not all mandatory fields are filled in',
         });
     }
 
 
     res.status(200).send({
-        message:'Successfully Aufgabe geaddet'
+        message: 'Successfully Aufgabe geaddet'
     });
 });
 
+
+app.get('/aufgaben', function (req: Request, res: Response) {
+    let query: string = "SELECT * FROM aufgaben";
+    database.query(query, (err: MysqlError, result: any) => {
+        if (err) {
+            //Query could not be executed
+            res.status(500).send({
+                message: 'Database request failed' + err,
+            })
+        } else {
+            //let aufgaben : Aufgabe[] = []
+            for (let row of result){
+                /*
+                let aufgabe = new Aufgabe(row.name, row.prioritaet)
+                aufgaben.push(aufgabe)
+                )
+                 */
+            }
+            res.status(200).send({
+                //aufgaben: aufgaben
+            })
+        }
+        }
+    )
+    })
 
 /*
 SQL Queries
