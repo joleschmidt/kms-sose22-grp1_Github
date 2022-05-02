9b25aab32d956090b00a423d8f042b9cdbbb217d
Niklas Schimmack <Niklas.Schimmack@mni.thm.de>
2022-05-02 12:36:07 +0200

    Habe den Ajax Befehl für das Bearbeiten gemacht. Es fehlt aber noch das Bearbeiten Fenster und die Eventfunktion dafür (schon bissi vorbereitet).

--- src/client.ts
+++ src/client.ts
@@ -26,9 +26,11 @@ function renderTodos (aufgaben: any[]) {
         const tableEntry: JQuery = $(`
         <div class="card m-3 mt-3" style="width: 18rem;">
         <div class="card-body">
-        <h5 class="card-title">${aufgabe.name}</h5>
+        <h5 class="card-title aufgaben_name">${aufgabe.name}</h5>
+            <h6 class="aufgaben_id">Aufgabe ${aufgabe.id}</h6>
+            <p class="card-text priority">Priorität ${aufgabe.priority}</p>
             <p class="card-text">${aufgabe.time}</p>
-            <a class="btn btn-secondary mr-2">Edit</a>
+            <a class="btn btn-secondary mr-2 aufgabeBearbeiten">Edit</a>
             <a class="btn btn-danger">Delete</a>
             </div>
             </div>
@@ -37,8 +39,34 @@ function renderTodos (aufgaben: any[]) {
     }
 }
 
+function updateToDos(event: Event){
+    const aufgaben_id: number = Number($(event.currentTarget as HTMLElement).parent().children(".aufgaben_id").val());
+    const name: string = String($(".bearbeitenInput").val());
+    const priority: number = Number($(".bearbeitenPrio").val());
+    $.ajax("/aufgabe/:" + aufgaben_id, {
+        method: "PUT",
+        contentType: "application/json",
+        data: JSON.stringify({
+            name: name,
+            priority: priority
+        })
+    }).then((data) =>{
+        alert(data)
+    }).catch((jqXHR: JQueryXHR) =>{
+        console.log(jqXHR.responseText);
+    })
+}
+
 //Main Callback
 $(() => {
+    let updateButton: JQuery = $(".aufgabeBearbeiten");
+    let saveChanges: JQuery = $("#saveChanges");
+    updateButton.on("click", function (){
+        //showUpdate(event);
+    });
+    saveChanges.on("click", function (){
+        updateToDos(event);
+    });
     getTodos();
 });
 
