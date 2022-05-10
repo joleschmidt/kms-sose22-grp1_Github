09c7af4a20e390f28e956fb34b7ff32190b0f4b0
Andreas <andreas-wagner2@mni.thm.de>
2022-05-10 18:30:56 +0200

    Added test

---
+++ test/server_test.js
@@ -0,0 +1,21 @@
+var chai = require('chai');
+let chaiHttp = require('chai-http')
+//var testDir = require('../src/client');
+let should = chai.should();
+var serverDir = require('../server');
+
+chai.use(chaiHttp);
+
+describe('/POST', ()=>{
+    it('Create message should succeed', (done) => {
+        chai.request(serverDir)
+            .post('/aufgabe')
+            .set('content-type', 'application/json')
+            .send('Test Aufgabe')
+            .end((err, res) => {
+                should.exist(res.body);
+                res.should.have.status(200);
+                done();
+            })
+    })
+})
