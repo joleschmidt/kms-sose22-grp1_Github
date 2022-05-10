ca0a11b743f139d01c8af37ee3755166e9cf4674
joleschmidt <janoleschmidt@outlook.com>
2022-05-10 16:57:39 +0200

    rebase

---
+++ build/src/client.d.ts
@@ -0,0 +1,5 @@
+declare function getTodos(): void;
+declare function renderTodos(aufgaben: any[]): void;
+declare function renderModal(event: any): void;
+declare function updateToDos(event: any): void;
+declare function createTask(inputAufgabe: any): void;
