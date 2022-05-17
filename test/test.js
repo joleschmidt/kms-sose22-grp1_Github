69d84879472786c816d12950b7ba8b768665c3c7
ziyad <Fatima1980!>
2022-05-17 12:58:17 +0200

    test 20

---
+++ test/test.js
@@ -0,0 +1,26 @@
+"use strict";
+exports.__esModule = true;
+require("mocha");
+var chai = require("chai");
+var chaiHTTP = require("chai-http");
+var server = require("../server");
+chai.should();
+chai.use(chaiHTTP);
+describe('"Task"', function () {
+    //alle Tests hier einfügen
+    describe('"Post /aufgabe"', function () {
+        it("Aufgabe erstellen", function (done) {
+            var aufgabe = {
+                aufgabe: "Die Welt retten"
+            };
+            chai.request(server)
+                .post("/aufgabe")
+                .send(aufgabe)
+                .end(function (err, res) {
+                res.should.have.status(200);
+                done();
+            });
+        });
+    });
+});
+//# sourceMappingURL=test.js.map
