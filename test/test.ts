ba71fb00038b4181527c10780ae0bb3a5ff40c33
ziyad <Fatima1980!>
2022-05-17 11:39:31 +0200

    test.ts erstellt

---
+++ test/test.ts
@@ -0,0 +1,16 @@
+import "mocha";
+
+const chai = require("chai");
+const chaiHTTP = require("chai-http");
+const server = require("../server");
+
+chai.should();
+chai.use(chaiHTTP);
+
+describe('"Task"', () => {
+    //alle Tests hier einfügen
+
+
+
+
+});
