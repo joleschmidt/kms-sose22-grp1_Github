b979b8f06e1be01d37d42ff3f128193fe2172a83
davidb <david.bausch@mni.thm.de>
2022-05-19 13:37:54 +0200

    test 36

---
+++ model/aufgabe.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+exports.Aufgabe = void 0;
+var Aufgabe = /** @class */ (function () {
+    function Aufgabe(id, name, time, prioritaet) {
+        this.id = id;
+        this.name = name;
+        this.time = time;
+        this.prioritaet = prioritaet;
+    }
+    return Aufgabe;
+}());
+exports.Aufgabe = Aufgabe;
+//# sourceMappingURL=aufgabe.js.map
