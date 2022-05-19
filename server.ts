755e2c9dff3a965b22eaacb52da85b4204855141
David Bausch <david.bausch@mni.thm.de>
2022-05-19 20:57:46 +0200

    password test 6

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ app.listen(8080, "localhost", function () {
 const database: mysql.Connection = mysql.createConnection({
     database: "kms_sose22_grp1",
     host: "localhost",
-    user: "root",
+    user: "root"
 });
 
 database.connect((err: MysqlError) => {
