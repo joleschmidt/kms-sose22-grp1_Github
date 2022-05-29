ee5b3bf7fd4fab7b4fdb764ea6e1a4b6f574f53c
Johanna Heiler <johanna.heiler@mni.thm.de>
2022-05-29 17:47:01 +0200

    added GET /aufgaben test

--- test/test.ts
+++ test/test.ts
@@ -3,12 +3,11 @@ import "mocha";
 import chai = require("chai");
 import chaiHTTP = require("chai-http");
 import server = require("../server.js");
-import {Aufgabe} from "../model/aufgabe";
+import {expect} from "chai";
 
 chai.should();
 chai.use(chaiHTTP);
 
-describe('"Task"', () => {
     //alle Tests hier einfügen
     describe('"Post /aufgabe"', () => {
         it("Aufgabe erstellen", (done) => {
@@ -18,14 +17,14 @@ describe('"Task"', () => {
             chai.request(server)
                 .post("/aufgabe")
                 .send(aufgabe)
-                .end((err: any, res: any) => {
+                .end((err, res) => {
                     res.should.have.status(200);
                     done();
                 })
             it("In der Post-Route wurden nicht alle Felder ausgefühlt", (done) => {
                 chai.request(server)
                     .post("/aufgabe")
-                    .end((err: any, res: any) => {
+                    .end((err, res) => {
                         res.should.have.status(400);
                         done();
                     })
@@ -33,4 +32,15 @@ describe('"Task"', () => {
         })
     });
 
+describe('GET /aufgaben', ()=> {
+    it('should send the success message and a list of aufgaben', (done) => {
+        chai.request(server)
+            .get('/aufgaben')
+            .set('dataType', 'json')
+            .end(function (err, res) {
+                expect(res).to.have.status(200);
+                res.body.should.have.property('aufgaben');
+                done();
+            });
+    });
 });
