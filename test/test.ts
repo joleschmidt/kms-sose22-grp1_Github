bb0870dbac834114b0dec54ac860a8ca07b048ee
Jan Ole Schmidt <44023730+joleschmidt@users.noreply.github.com>
2022-08-16 16:53:42 +0200

    Test GET-Route: Alle aufgaben zurückgeben

--- test/test.ts
+++ test/test.ts
@@ -9,6 +9,27 @@ chai.use(chaiHTTP);
 
 describe('"Task"', () => {
     //alle Tests hier einfügen
+    
+    // Get-Route Test
+    describe('GET /aufgaben', () => {
+        it('Alle Aufgaben zurückgeben', (done) => {
+            chai.request(server)
+                .get('/aufgaben')
+                .end((err: any, res: any) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+        it("Nicht alle Aufgaben ausgeben", (done) => {
+            chai.request(server)
+                .post("/aufgab") //Fehlerhafte URL
+                .end((err: any, res: any) => {
+                    res.should.have.status(404);
+                    done();
+                })
+        })
+    })
+    
     //Das ist der Post Test
     describe('"Post /aufgabe"', () => {
         it("Aufgabe erstellen", (done) => {
